0f1aafc2bb1a153fa6a051cd4712df414ec8eb29
A78009866 <ibispaintxarabic@gmail.com>
2026-05-13 11:05:21 +0000

    Upload via git-zip

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ const r2Client = new S3Client({
     accessKeyId: R2_ACCESS_KEY_ID || '',
     secretAccessKey: R2_SECRET_ACCESS_KEY || '',
   },
+  forcePathStyle: true,
 });
 
 const DEFAULT_PROFILE_PIC_URL = '/default_profile.png';
