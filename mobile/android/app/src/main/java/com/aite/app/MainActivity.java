a192170ad126bf3bb017b0d84c11f88f465e9df7
A78009866 <ibispaintxarabic@gmail.com>
2026-07-21 12:34:41 +0100

    Upload via git-zip - mobile/android/app/src/main/java/com/aite/app/MainActivity.java

--- mobile/android/app/src/main/java/com/aite/app/MainActivity.java
+++ mobile/android/app/src/main/java/com/aite/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.aite.app;
 
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    WebView webView = getBridge().getWebView();
+    if (webView != null) {
+      WebSettings settings = webView.getSettings();
+      settings.setSupportZoom(false);
+      settings.setBuiltInZoomControls(false);
+      settings.setDisplayZoomControls(false);
+      // Prevent text selection / context menu at the native level.
+      webView.setOnLongClickListener(new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+          return true;
+        }
+      });
+      webView.setLongClickable(false);
+    }
+  }
+}
